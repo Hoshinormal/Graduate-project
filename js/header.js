6c18f0c6b6119ba537a1e6b560a4d01439f0e676
Hohisaki <h20021207@gmail.com>
2025-11-24 13:52:53 +0800

    header模組化至main，成功。

---
+++ js/header.js
@@ -0,0 +1,6 @@
+fetch('./components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
