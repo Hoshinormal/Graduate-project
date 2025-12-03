53e7b9f106aaa6708ef12ba01e419a97e383eb52
Hohisaki <h20021207@gmail.com>
2025-12-03 17:21:02 +0800

    新增faq動畫

--- js/faq.js
+++ js/faq.js
@@ -22,3 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 }); 
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+    const elements =document.querySelectorAll('.fade-up, .fade-in');
+    elements.forEach((el,index) => {
+        if(el.classList.contains('fade-item')) {
+            let delayTime =0.2 +(index*0.15);
+            el.style.transitionDelay = `${delayTime}s`;
+        }
+        
+        setTimeout(() => {
+            el.classList.add('visible');
+        },100);
+    });
+});
