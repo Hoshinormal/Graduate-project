2282b45886820ba9dda8ca8ed05a6a603d1b84f9
chienyang0210 <11144263@cycu.org.tw>
2025-12-04 18:19:03 +0800

    50間公司的官網以及佔比

--- stock.js
+++ stock.js
@@ -9,7 +9,7 @@ const factorDetails = {
         industry: '半導體',
         short: '全球最大的晶圓代工廠',
         feature: '技術領先、市場占有率高',
-        taiwan50: '8.5%',
+        taiwan50: '61.53%',
         trend: '長期穩健成長',
         chart: '<img src="img/2330.png" style="max-width:100%;height:auto;" />',
         url: 'https://www.tsmc.com'
@@ -19,7 +19,7 @@ const factorDetails = {
         industry: '金融業',
         short: '以銀行及保險業務為主的金融集團',
         feature: '資本穩健、金融服務多元',
-        taiwan50: '1.5%',
+        taiwan50: '0.71%',
         trend: '推動數位金融與海外布局',
         chart: '<img src="charts/2887.png" alt="2887 台新新光金股價圖">',
         url: 'https://www.taishin.com.tw'
@@ -29,37 +29,37 @@ const factorDetails = {
         industry: '金融業',
         short: '中信金控',
         feature: '穩健經營',
-        taiwan50: '1.8%',
+        taiwan50: '1.34%',
         trend: '穩定',
         chart: '-',
-        url: '#'
+        url: 'https://www.ctbcholding.com/'
     },
     '2883凱基金': {
         price: '20',
         industry: '金融業',
         short: '凱基金控',
         feature: '多元業務',
-        taiwan50: '-',
+        taiwan50: '0.43%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.kgi.com/'
     },
     '2884玉山金': {
         price: '22',
         industry: '金融業',
         short: '玉山金控',
         feature: '銀行與證券業務',
-        taiwan50: '-',
+        taiwan50: '0.81%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.esunfhc.com/'
     },
     '2317鴻海': {
         price: '105',
         industry: '電子',
         short: '全球代工巨頭',
         feature: '製造能力強',
-        taiwan50: '4.2%',
+        taiwan50: '4.8%',
         trend: '成長穩健',
         chart: '-',
         url: 'https://www.foxconn.com'
@@ -69,27 +69,27 @@ const factorDetails = {
         industry: '金融業',
         short: '永豐金控',
         feature: '銀行與證券',
-        taiwan50: '-',
+        taiwan50: '0.56%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.sinotrade.com.tw/'
     },
     '2886兆豐金': {
         price: '28',
         industry: '金融業',
         short: '兆豐金控',
         feature: '銀行及保險',
-        taiwan50: '-',
+        taiwan50: '0.82%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.megaholdings.com.tw/'
     },
     '2303聯電': {
         price: '60',
         industry: '半導體',
         short: '晶圓代工',
         feature: '穩定技術',
-        taiwan50: '1.2%',
+        taiwan50: '0.96%',
         trend: '-',
         chart: '-',
         url: 'https://www.umc.com'
@@ -99,7 +99,7 @@ const factorDetails = {
         industry: '鋼鐵',
         short: '中鋼公司',
         feature: '鋼鐵製造',
-        taiwan50: '-',
+        taiwan50: '0.37%',
         trend: '-',
         chart: '-',
         url: 'https://www.csc.com.tw'
@@ -109,37 +109,37 @@ const factorDetails = {
         industry: '金融業',
         short: '元大金控',
         feature: '證券與銀行',
-        taiwan50: '-',
+        taiwan50: '0.69%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.yuanta.com/'
     },
     '2892第一金': {
         price: '19',
         industry: '金融業',
         short: '第一金控',
         feature: '銀行業務',
-        taiwan50: '-',
+        taiwan50: '0.52',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.firstholding.com.tw/'
     },
     '5880合庫金': {
         price: '21',
         industry: '金融業',
         short: '合作金庫金控',
         feature: '銀行及投資',
-        taiwan50: '-',
+        taiwan50: '0.43%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.tcfhc.com.tw/'
     },
     '2882國泰金': {
         price: '50',
         industry: '金融業',
         short: '國泰金控',
         feature: '保險與銀行',
-        taiwan50: '2.3%',
+        taiwan50: '1.09%',
         trend: '穩定成長',
         chart: '-',
         url: 'https://www.cathayholdings.com.tw'
@@ -149,17 +149,17 @@ const factorDetails = {
         industry: '金融業',
         short: '華南金控',
         feature: '銀行業務',
-        taiwan50: '-',
+        taiwan50: '0.49%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.hnfhc.com.tw/'
     },
     '2881富邦金': {
         price: '55',
         industry: '金融業',
         short: '富邦金控',
         feature: '保險與投資',
-        taiwan50: '-',
+        taiwan50: '1.35%',
         trend: '-',
         chart: '-',
         url: 'https://www.fubon.com'
@@ -169,7 +169,7 @@ const factorDetails = {
         industry: '化學',
         short: '南亞塑膠',
         feature: '化學原料',
-        taiwan50: '-',
+        taiwan50: '0.58%',
         trend: '-',
         chart: '-',
         url: 'https://www.nanya.com.tw'
@@ -179,7 +179,7 @@ const factorDetails = {
         industry: '食品',
         short: '統一企業',
         feature: '食品與飲料',
-        taiwan50: '-',
+        taiwan50: '0.64%',
         trend: '-',
         chart: '-',
         url: 'https://www.uni-president.com.tw'
@@ -189,7 +189,7 @@ const factorDetails = {
         industry: '化學',
         short: '台塑集團',
         feature: '化學製造',
-        taiwan50: '-',
+        taiwan50: '0.32%',
         trend: '-',
         chart: '-',
         url: 'https://www.fpg.com.tw'
@@ -199,7 +199,7 @@ const factorDetails = {
         industry: '電信',
         short: '中華電信',
         feature: '電信服務',
-        taiwan50: '-',
+        taiwan50: '0.86%',
         trend: '-',
         chart: '-',
         url: 'https://www.cht.com.tw'
@@ -209,17 +209,17 @@ const factorDetails = {
         industry: '銀行',
         short: '上海商業銀行',
         feature: '銀行業務',
-        taiwan50: '-',
+        taiwan50: '0.26%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.scsb.com.tw/'
     },
     '3711日月光投控': {
         price: '105',
         industry: '半導體',
         short: '日月光投控',
         feature: '封測與代工',
-        taiwan50: '-',
+        taiwan50: '1.3%',
         trend: '-',
         chart: '-',
         url: 'https://www.aspeed.com.tw'
@@ -229,7 +229,7 @@ const factorDetails = {
         industry: '電子',
         short: '緯創資通',
         feature: '電子製造',
-        taiwan50: '-',
+        taiwan50: '0.77%',
         trend: '-',
         chart: '-',
         url: 'https://www.wistron.com'
@@ -239,7 +239,7 @@ const factorDetails = {
         industry: '電子',
         short: '廣達電腦',
         feature: '筆電製造',
-        taiwan50: '-',
+        taiwan50: '1.34%',
         trend: '-',
         chart: '-',
         url: 'https://www.quanta.com'
@@ -249,7 +249,7 @@ const factorDetails = {
         industry: '電子',
         short: '光寶科技',
         feature: '電子零組件',
-        taiwan50: '-',
+        taiwan50: '0.54%',
         trend: '-',
         chart: '-',
         url: 'https://www.liteon.com'
@@ -259,7 +259,7 @@ const factorDetails = {
         industry: '電子',
         short: '和碩聯合科技',
         feature: '代工組裝',
-        taiwan50: '-',
+        taiwan50: '0.24%',
         trend: '-',
         chart: '-',
         url: 'https://www.pegatroncorp.com'
@@ -269,7 +269,7 @@ const factorDetails = {
         industry: '電子',
         short: '台達電子',
         feature: '電源管理與自動化',
-        taiwan50: '-',
+        taiwan50: '3.34%',
         trend: '-',
         chart: '-',
         url: 'https://www.deltaww.com'
@@ -279,7 +279,7 @@ const factorDetails = {
         industry: '電信',
         short: '遠傳電信',
         feature: '電信服務',
-        taiwan50: '-',
+        taiwan50: '0.27%',
         trend: '-',
         chart: '-',
         url: 'https://www.fetnet.net.tw'
@@ -289,7 +289,7 @@ const factorDetails = {
         industry: '運輸',
         short: '陽明海運',
         feature: '航運服務',
-        taiwan50: '-',
+        taiwan50: '0.16%',
         trend: '-',
         chart: '-',
         url: 'https://www.yangming.com'
@@ -299,7 +299,7 @@ const factorDetails = {
         industry: '電子',
         short: '國巨',
         feature: '被動元件製造',
-        taiwan50: '-',
+        taiwan50: '0.63%',
         trend: '-',
         chart: '-',
         url: 'https://www.yageo.com'
@@ -309,17 +309,17 @@ const factorDetails = {
         industry: '金融',
         short: '中租控股',
         feature: '租賃服務',
-        taiwan50: '-',
+        taiwan50: '0.26%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.chaileaseholding.com/'
     },
     '2454聯發科': {
         price: '1000',
         industry: '半導體',
         short: '聯發科技',
         feature: 'IC設計',
-        taiwan50: '-',
+        taiwan50: '3.57%',
         trend: '-',
         chart: '-',
         url: 'https://www.mediatek.com'
@@ -329,7 +329,7 @@ const factorDetails = {
         industry: '電信',
         short: '台灣大哥大',
         feature: '電信服務',
-        taiwan50: '-',
+        taiwan50: '0.26%',
         trend: '-',
         chart: '-',
         url: 'https://www.taiwanmobile.com'
@@ -339,7 +339,7 @@ const factorDetails = {
         industry: '運輸',
         short: '萬海航運',
         feature: '航運服務',
-        taiwan50: '-',
+        taiwan50: '0.2%',
         trend: '-',
         chart: '-',
         url: 'https://www.tonymarine.com'
@@ -349,7 +349,7 @@ const factorDetails = {
         industry: '化學',
         short: '台塑化',
         feature: '化學製造',
-        taiwan50: '-',
+        taiwan50: '0.11%',
         trend: '-',
         chart: '-',
         url: 'https://www.fpc.com.tw'
@@ -359,7 +359,7 @@ const factorDetails = {
         industry: '運輸',
         short: '長榮海運',
         feature: '航運服務',
-        taiwan50: '-',
+        taiwan50: '0.34%',
         trend: '-',
         chart: '-',
         url: 'https://www.evergreen-marine.com'
@@ -369,17 +369,17 @@ const factorDetails = {
         industry: '生技',
         short: '康霈生技',
         feature: '生技藥品',
-        taiwan50: '-',
+        taiwan50: '0.28%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.caliwaybiopharma.com/'
     },
     '2357華碩': {
         price: '500',
         industry: '電子',
         short: '華碩電腦',
         feature: '筆電與主機板',
-        taiwan50: '-',
+        taiwan50: '0.73',
         trend: '-',
         chart: '-',
         url: 'https://www.asus.com'
@@ -389,7 +389,7 @@ const factorDetails = {
         industry: '電子',
         short: '聯詠科技',
         feature: '顯示晶片',
-        taiwan50: '-',
+        taiwan50: '0.39%',
         trend: '-',
         chart: '-',
         url: 'https://www.pixart.com.tw'
@@ -399,7 +399,7 @@ const factorDetails = {
         industry: '零售',
         short: '統一超商',
         feature: '便利商店',
-        taiwan50: '-',
+        taiwan50: '0.22%',
         trend: '-',
         chart: '-',
         url: 'https://www.7-11.com.tw'
@@ -409,7 +409,7 @@ const factorDetails = {
         industry: '電子',
         short: '智邦科技',
         feature: '網通設備',
-        taiwan50: '-',
+        taiwan50: '0.85%',
         trend: '-',
         chart: '-',
         url: 'https://www.abil.com.tw'
@@ -419,7 +419,7 @@ const factorDetails = {
         industry: '電子',
         short: '瑞昱半導體',
         feature: '網路晶片',
-        taiwan50: '-',
+        taiwan50: '0.45%',
         trend: '-',
         chart: '-',
         url: 'https://www.realtek.com'
@@ -429,7 +429,7 @@ const factorDetails = {
         industry: '電子',
         short: '研華科技',
         feature: '工業自動化',
-        taiwan50: '-',
+        taiwan50: '0.23%',
         trend: '-',
         chart: '-',
         url: 'https://www.advantech.com'
@@ -439,47 +439,47 @@ const factorDetails = {
         industry: '電子',
         short: '奇鋐科技',
         feature: 'IC設計',
-        taiwan50: '-',
+        taiwan50: '0.77%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.avc.co/zh-tw/'
     },
     '2383台光電': {
         price: '55',
         industry: '電子',
         short: '台光電',
         feature: '光電元件',
-        taiwan50: '-',
+        taiwan50: '0.69%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.emctw.com/zh-TW'
     },
     '2207和泰車': {
         price: '350',
         industry: '汽車',
         short: '和泰汽車',
         feature: '汽車製造與銷售',
-        taiwan50: '-',
+        taiwan50: '0.28%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.hotaimotor.com.tw/'
     },
     '6669緯穎': {
         price: '600',
         industry: '電子',
         short: '緯穎科技',
         feature: '伺服器代工',
-        taiwan50: '-',
+        taiwan50: '0.83%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.wiwynn.com/'
     },
     '3008大立光': {
         price: '7000',
         industry: '光學',
         short: '大立光電',
         feature: '光學鏡頭',
-        taiwan50: '-',
+        taiwan50: '0.36%',
         trend: '-',
         chart: '-',
         url: 'https://www.largan.com'
@@ -489,20 +489,20 @@ const factorDetails = {
         industry: '半導體',
         short: '世芯科技',
         feature: 'IC設計',
-        taiwan50: '-',
+        taiwan50: '0.44%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.alchip.com/tw'
     },
     '2059川湖': {
         price: '35',
         industry: '電子',
         short: '川湖科技',
         feature: '被動元件',
-        taiwan50: '-',
+        taiwan50: '0.34%',
         trend: '-',
         chart: '-',
-        url: '#'
+        url: 'https://www.kingslide.com/'
     }
 }
 
@@ -523,7 +523,7 @@ function renderDetail(key) {
     <p><strong>一句介紹：</strong> ${data.short || '-'}</p>
     <p><strong>特色：</strong> ${data.feature || '-'}</p>
     <p><strong>台灣50占比：</strong> ${data.taiwan50 || '-'}</p>
-    <p><strong>未來趨勢：</strong> ${data.trend || '-'}</p>
+    
     <p><strong>股價圖表：</strong> ${data.chart || '-'}</p>
     <p><strong>公司網址：</strong> <a href="${data.url || '#'}" target="_blank">${data.url || '-'}</a></p>
   `;
